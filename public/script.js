fbe79a88f0e811614c9cb1c2f689ae180ed7d62b
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2019-06-19 17:02:08 +0800

    use xmlhttprequest instead of fetch

--- public/script.js
+++ public/script.js
@@ -1,36 +1,72 @@
 (function app() {
-  'use strict';
+  "use strict";
+
+  function noop() {}
+
+  function ajax(url, options) {
+    var opts = options || {};
+    var onSuccess = opts.onSuccess || noop;
+    var onError = opts.onError || noop;
+    var dataType = opts.dataType || "json";
+    var method = opts.method || "GET";
+
+    var request = new XMLHttpRequest();
+    request.open(method, url);
+    if (dataType === "json") {
+      request.overrideMimeType("application/json");
+      request.responseType = "json";
+      request.setRequestHeader("Accept", "application/json");
+    }
+
+    request.onload = function() {
+      if (request.status >= 200 && request.status < 400) {
+        onSuccess(request.response);
+      } else {
+        onError(request.response);
+      }
+    };
+
+    request.onerror = onError;
+
+    request.send(opts.body);
+  }
 
   function $(selector) {
     return document.querySelector(selector);
   }
 
   var currentPage = 1;
-  var $loadPokemonBtn = $('#load-pokemon-btn');
+  var $loadPokemonBtn = $("#load-pokemon-btn");
 
-  function getPokemons(page = currentPage, limit = 15) {
-    return fetch(
-      `https://pokemon-json.herokuapp.com/api/pokedex?_page=${page}&_limit=${limit}`
-    ).then(function(res) {
-      if (res.ok) {
-        return res.json();
+  function getPokemons(options) {
+    const page = options.page || currentPage;
+    const limit = options.limit || 15;
+
+    ajax(
+      `https://pokemon-json.herokuapp.com/api/pokedex?_page=${page}&_limit=${limit}`,
+      {
+        onSuccess: options.onSuccess,
+        onError: options.onError
       }
-      throw res.body;
-    });
+    );
   }
 
   function createPokemonEl(pokemon) {
-    const tmpl = $('#pokemon-record');
+    const tmpl = $("#pokemon-record");
     const $el = document.importNode(tmpl.content, true);
 
-    $el.querySelector('.pokemon-name').textContent = `#${pokemon.id} ${pokemon.name.english}`;
-    $el.querySelector('.pokemon-thumbnail').setAttribute('src', pokemon.thumbnail);
+    $el.querySelector(".pokemon-name").textContent = `#${pokemon.id} ${
+      pokemon.name.english
+    }`;
+    $el
+      .querySelector(".pokemon-thumbnail")
+      .setAttribute("src", pokemon.thumbnail);
 
     return $el;
   }
 
   function render(pokemons) {
-    const $target = $('#pokemon-container');
+    const $target = $("#pokemon-container");
 
     pokemons.forEach(function(pokemon) {
       const $pokemon = createPokemonEl(pokemon);
@@ -42,18 +78,20 @@
 
   function loadPokemons() {
     $loadPokemonBtn.disabled = true;
-    getPokemons()
-      .then(render)
-      .then(function(pokemons) {
+    getPokemons({
+      onSuccess: function(pokemons) {
+        render(pokemons);
+
         if (pokemons.length > 0) {
           currentPage++;
           $loadPokemonBtn.disabled = false;
         } else {
           $loadPokemonBtn.parentNode.removeChild($loadPokemonBtn);
         }
-      });
+      }
+    });
   }
 
-  $loadPokemonBtn.addEventListener('click', loadPokemons);
+  $loadPokemonBtn.addEventListener("click", loadPokemons);
   loadPokemons();
 })();
