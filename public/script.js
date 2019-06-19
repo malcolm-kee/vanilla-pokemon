3cdd250473e622dcd225dd26e6ccb3c8051638ac
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2019-06-19 23:00:44 +0800

    add fav function

--- public/script.js
+++ public/script.js
@@ -1,5 +1,5 @@
 (function app() {
-  "use strict";
+  'use strict';
 
   function noop() {}
 
@@ -7,15 +7,15 @@
     var opts = options || {};
     var onSuccess = opts.onSuccess || noop;
     var onError = opts.onError || noop;
-    var dataType = opts.dataType || "json";
-    var method = opts.method || "GET";
+    var dataType = opts.dataType || 'json';
+    var method = opts.method || 'GET';
 
     var request = new XMLHttpRequest();
     request.open(method, url);
-    if (dataType === "json") {
-      request.overrideMimeType("application/json");
-      request.responseType = "json";
-      request.setRequestHeader("Accept", "application/json");
+    if (dataType === 'json') {
+      request.overrideMimeType('application/json');
+      request.responseType = 'json';
+      request.setRequestHeader('Accept', 'application/json');
     }
 
     request.onload = function() {
@@ -35,38 +35,36 @@
     return document.querySelector(selector);
   }
 
+  function toggleClass($el, className) {
+    $el && $el.classList.toggle(className);
+  }
+
   var currentPage = 1;
-  var $loadPokemonBtn = $("#load-pokemon-btn");
+  var $loadPokemonBtn = $('#load-pokemon-btn');
+  var $pokemonContainer = $('#pokemon-container');
 
   function getPokemons(options) {
     const page = options.page || currentPage;
     const limit = options.limit || 15;
 
-    ajax(
-      `https://pokemon-json.herokuapp.com/api/pokedex?_page=${page}&_limit=${limit}`,
-      {
-        onSuccess: options.onSuccess,
-        onError: options.onError
-      }
-    );
+    ajax('https://pokemon-json.herokuapp.com/api/pokemons?_page=' + page + '&_limit=' + limit, {
+      onSuccess: options.onSuccess,
+      onError: options.onError
+    });
   }
 
   function createPokemonEl(pokemon) {
-    const tmpl = $("#pokemon-record");
+    const tmpl = $('#pokemon-record');
     const $el = document.importNode(tmpl.content, true);
 
-    $el.querySelector(".pokemon-name").textContent = `#${pokemon.id} ${
-      pokemon.name.english
-    }`;
-    $el
-      .querySelector(".pokemon-thumbnail")
-      .setAttribute("src", pokemon.thumbnail);
+    $el.querySelector('.pokemon-name').textContent = `#${pokemon.id} ${pokemon.name}`;
+    $el.querySelector('.pokemon-thumbnail').setAttribute('src', pokemon.thumbnail);
 
     return $el;
   }
 
   function render(pokemons) {
-    const $target = $("#pokemon-container");
+    const $target = $('#pokemon-container');
 
     pokemons.forEach(function(pokemon) {
       const $pokemon = createPokemonEl(pokemon);
@@ -92,6 +90,14 @@
     });
   }
 
-  $loadPokemonBtn.addEventListener("click", loadPokemons);
+  function onPokemonClick(ev) {
+    const $pokemon = ev.target.closest('.pokemon');
+
+    toggleClass($pokemon && $pokemon.querySelector('.fav-icon'), 'is-empty');
+  }
+
   loadPokemons();
+
+  $loadPokemonBtn.addEventListener('click', loadPokemons);
+  $pokemonContainer.addEventListener('click', onPokemonClick);
 })();
