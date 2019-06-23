382305179582c784579fab927651e565e1e82c08
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2019-06-23 18:14:30 +0800

    focus pokemon and show details

--- public/script.js
+++ public/script.js
@@ -39,26 +39,40 @@
     $el && $el.classList.toggle(className);
   }
 
-  var currentPage = 1;
   var $loadPokemonBtn = $('#load-pokemon-btn');
   var $pokemonContainer = $('#pokemon-container');
 
-  function getPokemons(options) {
-    const page = options.page || currentPage;
-    const limit = options.limit || 15;
+  var getPokemons = (function() {
+    var currentPage = 1;
 
-    ajax('https://pokemon-json.herokuapp.com/api/pokemons?_page=' + page + '&_limit=' + limit, {
-      onSuccess: options.onSuccess,
-      onError: options.onError
-    });
-  }
+    return function getPokemonsCall(options) {
+      const limit = options.limit || 15;
+      const onSuccess = options.onSuccess || noop;
+
+      ajax(
+        'https://pokemon-json.herokuapp.com/api/pokemons?_page=' + currentPage + '&_limit=' + limit,
+        {
+          onSuccess: function(res) {
+            currentPage++;
+            onSuccess(res);
+          },
+          onError: options.onError
+        }
+      );
+    };
+  })();
 
   function createPokemonEl(pokemon) {
-    const tmpl = $('#pokemon-record');
-    const $el = document.importNode(tmpl.content, true);
+    var tmpl = $('#pokemon-record');
+    var $el = document.importNode(tmpl.content, true);
 
     $el.querySelector('.pokemon-name').textContent = `#${pokemon.id} ${pokemon.name}`;
     $el.querySelector('.pokemon-thumbnail').setAttribute('src', pokemon.thumbnail);
+
+    var $pokemon = $el.querySelector('.pokemon');
+
+    $pokemon.dataset.pokemonId = pokemon.id;
+    $pokemon.dataset.name = pokemon.name;
 
     return $el;
   }
@@ -81,7 +95,6 @@
         render(pokemons);
 
         if (pokemons.length > 0) {
-          currentPage++;
           $loadPokemonBtn.disabled = false;
         } else {
           $loadPokemonBtn.parentNode.removeChild($loadPokemonBtn);
@@ -90,14 +103,62 @@
     });
   }
 
-  function onPokemonClick(ev) {
-    const $pokemon = ev.target.closest('.pokemon');
+  function createPokemonDetails(pokemon) {
+    var tmpl = $('#pokemon-details');
+    var $el = document.importNode(tmpl.content, true);
 
-    toggleClass($pokemon && $pokemon.querySelector('.fav-icon'), 'is-empty');
+    $el.querySelector('.pokemon-image').setAttribute('src', pokemon.image);
+    $el.querySelector('.pokemon-names').textContent =
+      pokemon.name &&
+      pokemon.name.english + ' | ' + pokemon.name.japanese + ' | ' + pokemon.name.chinese;
+    $el.querySelector('.pokemon-types').textContent = pokemon.type.join(', ');
+    $el.querySelector('.pokemon-hp').textContent = pokemon.base.HP;
+    $el.querySelector('.pokemon-atk').textContent = pokemon.base.Attack;
+    $el.querySelector('.pokemon-def').textContent = pokemon.base.Defense;
+    $el.querySelector('.pokemon-sp-atk').textContent = pokemon.base['Sp. Attack'];
+    $el.querySelector('.pokemon-sp-def').textContent = pokemon.base['Sp. Defense'];
+    $el.querySelector('.pokemon-speed').textContent = pokemon.base.Speed;
+
+    return $el;
+  }
+
+  function focusPokemon(pokemon) {
+    var pokemonId = pokemon.pokemonId;
+    var $pokemonDialog = $('#pokemon-dialog');
+    toggleClass($pokemonDialog.querySelector('.loading-text'), 'is-shown');
+    toggleClass($pokemonDialog, 'is-open');
+
+    ajax('https://pokemon-json.herokuapp.com/api/pokedex/' + pokemonId, {
+      onSuccess: function(pokemon) {
+        var $details = createPokemonDetails(pokemon);
+        $pokemonDialog.querySelector('.pokemon-details').appendChild($details);
+        toggleClass($pokemonDialog.querySelector('.loading-text'), 'is-shown');
+      }
+    });
+  }
+
+  function unFocusPokemon() {
+    toggleClass($('#pokemon-dialog'), 'is-open');
+    $('#pokemon-dialog').querySelector('.pokemon-details').innerHTML = '';
+  }
+
+  function onPokemonClick(ev) {
+    var target = ev.target;
+
+    if (target.classList.contains('fav-icon')) {
+      var $pokemon = target.closest('.pokemon');
+      toggleClass($pokemon && $pokemon.querySelector('.fav-icon'), 'is-empty');
+    } else {
+      var $pokemon = target.closest('.pokemon');
+      if ($pokemon) {
+        focusPokemon($pokemon.dataset);
+      }
+    }
   }
 
   loadPokemons();
 
   $loadPokemonBtn.addEventListener('click', loadPokemons);
   $pokemonContainer.addEventListener('click', onPokemonClick);
+  $('#pokemon-dialog .cancel-btn').addEventListener('click', unFocusPokemon);
 })();
